8eeff2890bec38cac107dbbe793b3f713d501de7
waqar <syedwaqarulhassanshah6@gmial.com>
2023-10-09 17:03:49 +0500

    new changes 5 30pm

---
+++ object/itrerate-object.js
@@ -0,0 +1,15 @@
+// how to iterate object
+
+let personNew = {
+    name : "waqar",
+    age : 24,
+    hobbies : ["gaming","coding","content writing"]
+}
+
+// for in loop 
+// object.keys
+
+for(let key in personNew){
+    console.log(personNew[key])
+}
+
