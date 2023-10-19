ce11d81f7feb200b6284853497e9f7edf23836ef
waqar <syedwaqarulhassanshah6@gmial.com>
2023-10-19 12:21:54 +0500

    10/19/23

---
+++ functions/object-destruturing.js
@@ -0,0 +1,27 @@
+// object destruturing
+// let p = document.querySelector("p");
+
+// const person = {
+//     firstName : "waqar",
+//     gender : "male",
+//     age : 25,
+// }
+
+
+// function checkobjkeys(obj){
+//     console.log(obj.firstName);
+//     console.log(obj.gender);
+//     console.log(obj.age);
+  
+// }
+
+// function checkobjkeys({firstName,age}){
+//   p.innerHTML = `name ${firstName} age ${age}`;
+// }
+// checkobjkeys(person);
+// checkobjkeys(obj);
+
+// console.log(person.firstName,person.gender);
+
+
+
